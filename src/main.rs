b86a01fdb7b47c82574b17042a2d54d95165359d
Manank Patel <pmanank200502@gmail.com>
2022-10-25 02:19:04 +0530

    add major improvements

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,95 @@
-
-
 #![allow(unused)]
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
-use pest::Parser;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-#[derive(Parser)]
-#[grammar= "strace.pest"]
-pub struct Syscallpar;
-
-
 fn main() {
-    println!("Hello, world!");
-
     let mut fil = File::open("output.txt").expect("unable to open file");
-    //let mut fil = read_lines("output.txt").expect("unable to open file");
     let mut lines = io::BufReader::new(fil).lines();
     let mut cont = String::new();
-   // fil.read_to_string(&mut cont).expect("Failed");
-    
-    let mut binding = lines.next().unwrap().unwrap();
+
     let mut scalls = ["execve", "openat", "dup2", "unlinkat", "mkdir", "chdir"];
-    
-//    let successful_parse = Syscallpar::parse(Rule::syscall, "517604 execve(\"./tut.sh\", [\"./tut.sh\"], 0x7ffd69d77700 /* 44 vars */) = 0");
-    let successful_parse = Syscallpar::parse(Rule::syscall, &binding);
-//    let successful_parse = Syscallpar::parse(Rule::args, &binding);
-    let splitted: Vec<&str> = binding.split(" ").collect();
-    let splitted2: Vec<&str> = binding.split("\"").collect();
-    println!("{:?}", successful_parse);
-    println!("{:?}", splitted);
-    println!("{:?}", splitted2);
-    out_gen(&splitted, &mut String::new(), &scalls);
+
+    for line in lines {
+        match line {
+            Ok(s) => {
+                let splitted: Vec<&str> = s.split(" ").collect();
+                let splitted2: Vec<&str> = s.split("\"").collect();
+
+                out_gen(&splitted, &mut String::new(), &splitted2, &scalls)
+            }
+            _ => continue,
+        }
+    }
 }
 
+fn out_gen(vtr: &Vec<&str>, otp: &mut String, argm: &Vec<&str>, scalls: &[&str]) {
+    match vtr[1].find(scalls[0]) {
+        Some(j) => {
+            if argm[1] == "/usr/bin/ln" {
+                println!("Created hardlink from {} to {}", argm[5], argm[7]);
+                return;
+            }
+            if argm[1] == "/usr/bin/mkdir" {
+                println!("Created directory {}", argm[7]);
+                return;
+            }
+            if argm[1] == "/usr/bin/cat" {
+                println!("writing to file {} through cat", argm[5]);
+                return;
+            }
+            println!("Called execve with argument {}", argm[1]);
+            return;
+        }
+        _ => (),
+    };
 
-fn out_gen(vtr: &Vec<&str>, otp: &mut String, scalls: &[str]) {
-    println!("{:?}", vtr[1].find("execve"));
+    match vtr[1].find(scalls[1]) {
+        Some(j) => {
+            if let Some(x) = argm[1].find("/usr/lib/") {
+                return;
+            } else if let Some(x) = argm[1].find("/etc/") {
+                return;
+            }
+            println!("opened file {}", argm[1]);
+            return;
+        }
+        _ => (),
+    };
 
+    //    match vtr[1].find(scalls[2]) {
+    //        Some(j) => {
+    //            println!("duplicated file descripitor {}{}", vtr[1], vtr[2]);
+    //            return;
+    //        }
+    //        _ =>  ()
+    //    };
+
+    match vtr[1].find(scalls[3]) {
+        Some(j) => {
+            println!("deleted file {}", argm[1]);
+            return;
+        }
+        _ => (),
+    };
+
+    match vtr[1].find(scalls[5]) {
+        Some(j) => {
+            println!("change directory to {}", argm[1]);
+            return;
+        }
+        _ => (),
+    };
+
+    match vtr[1].find(scalls[4]) {
+        Some(j) => {
+            if argm[1] == "/usr/bin/mkdir" {
+                return;
+            }
+            println!("make directory {}", argm[1]);
+            return;
+        }
+        _ => (),
+    };
 }
